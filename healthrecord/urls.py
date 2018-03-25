75ef8ba03aac1c4eed96e617e8b38471bfc1997d
Kuldeep Prajapati <37729919+kuldeepkprajapati@users.noreply.github.com>
2018-03-25 15:18:28 +0530

    new function added in views

--- healthrecord/urls.py
+++ healthrecord/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('prajapati', views.summition, name='summition'),
+    path('prajapatip', views.index, name='index'),
 ]
--- healthrecord/views.py
+++ healthrecord/views.py
@@ -9,4 +9,10 @@ def summition(request):
     return HttpResponse( "kc")
     # return HttpResponse("Your sum is==>" + str(c))
 
+def index(request):
+   my_data = request.GET
+   a=float(my_data.get('a'))
+   b=float(my_data.get('b'))
+   c=a+b
+   return HttpResponse("Your sum is==>" + str(c))
 
